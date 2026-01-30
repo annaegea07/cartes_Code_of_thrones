fbf75ca6e508df7eea41eadee632faf225b46ee3
“Anna <anna.egea@gracia.lasalle.cat>
2026-01-30 13:35:31 +0100

    Constructor de baseEntitat definit i Getters i seters inclosos

--- baseEntitat.java
+++ baseEntitat.java
@@ -2,4 +2,38 @@ public class baseEntitat {
     private String nom;
     private int nivell;
     private int puntsVida;
+    //constructor
+    public baseEntitat(String nom, int nivell, int puntsVida){
+        this.nom=nom;
+        this.nivell=nivell;
+        this.puntsVida=puntsVida;
+    }
 }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNivell() {
+        return nivell;
+    }
+
+    public void setNivell(int n) {
+        this.nivell = n;
+    }
+
+    public int getPuntsVida() {
+        return puntsVida;
+    }
+
+    public void setPuntsVida(int v) {
+        this.puntsVida = v;
+    }
+
+
+
+
